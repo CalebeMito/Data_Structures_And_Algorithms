ab2a4b041db196d7f2b288d5dc9f074f90b9c797
Calebe Birer <90878309+CalebeMito@users.noreply.github.com>
2023-07-19 16:36:20 -0300

    Update Main.java
    Update project

--- Pilha (Stack)/Main.java
+++ Pilha (Stack)/Main.java
@@ -6,8 +6,23 @@ public class Main {
 		
 		Stack<String> stack = new Stack<String>();
 
-		//System.out.println(stack.empty()); //para verificar se a lista esta vazia - ira retornar FALSE se a listanao estiver mais vazia
+		// *******************************************************
+		  
+		  // stack = LIFO data structure. Last-In First-Out
+		  //               stores objects into a sort of "vertical tower"
+		  //               push() to add objects to the top
+		  //               pop() to remove objects from the top
+		  
+		  // uses of stacks?
+		  // 1. undo/redo features in text editors
+		  // 2. moving back/forward through browser history
+		  // 3. backtracking algorithms (maze, file directories)
+		  // 4. calling functions (call stack)
+		  
+		  // *******************************************************
+
 		
+		//System.out.println(stack.empty()); //para verificar se a lista esta vazia - ira retornar FALSE se a listanao estiver mais vazia
 		
 		stack.push("minecraft");//Adicionando objetos na lista
 		stack.push("CS");
@@ -19,8 +34,12 @@ public class Main {
 		
 		//stack.pop();//adiciona no lugar mais alto da lista 
 		
-		System.out.println(stack.search("fallout76"));
+		//System.out.println(stack.search("fallout76")); //ele procura na lista mas como nao existe ele retorna negativo
 		//System.out.println(MyFavGame);
+		
+		for(int i=0; i < 1000000; i++) {
+			stack.push("Fallout76");
+		}
 		
 	}
 }
